7c81d715a5a519ee0b8df9f3ba4caa322f8af8b7
KyleM73 <kyle.morgenstein@gmail.com>
2025-07-26 10:47:59 -0500

    decorator and wrapper workflow

--- cusadi/source/CusadiWrapper.py
+++ cusadi/source/CusadiWrapper.py
@@ -6,50 +6,53 @@ import casadi as ca
 import cusadi as cu
 
 class CusadiWrapper:
-    def __init__(self, fn: ca.Function, num_envs: int) -> None:
-        cu.generateCUDACodeDouble(fn)
-        # cu.generatePytorchCode(fn)
-        cu.generateCMakeLists(fn, dtype_str="double")
-        t_compile = time.time()
-        print("Compiling CUDA code...")
-        status = os.system(f"cd {cu.CUSADI_ROOT_DIR} && rm -rf build && mkdir -p build && cd build && cmake .. && make -j")
-        if status == 0:
-            print("Compilation complete.")
+    def __init__(self, fn_handle: Callable | ca.Function, num_envs: int, gen_pytorch: bool = False) -> None:
+        if not isinstance(fn_handle, ca.Function):
+            fn: ca.Function = fn_handle()
         else:
-            print("Compilation failed.")
-            del self
-            exit(1)
-        t_compile = time.time() - t_compile
-        print(f"Compilation time: {t_compile:.2f} seconds")
-        self.fn = cu.CusadiFunction(fn, num_envs)
-    
-    def __call__(self, inputs: list) -> torch.Tensor:
-        self.fn.evaluate(inputs)
-        torch.cuda.synchronize() # necessary?
-        return self.fn.getDenseOutput()
-
-def cusadi(num_envs: int) -> Callable:
-    def decorator(fn: ca.Function) -> Callable:
+            fn: ca.Function = fn_handle
         cu.generateCUDACodeDouble(fn)
-        # cu.generatePytorchCode(fn)
+        if gen_pytorch:
+            cu.generatePytorchCode(fn)
         cu.generateCMakeLists([fn], dtype_str="double")
         
-        # Compile the CUDA code
         t_compile = time.time()
         print("Compiling CUDA code...")
         status = os.system(f"cd {cu.CUSADI_ROOT_DIR} && rm -rf build && mkdir -p build && cd build && cmake .. && make -j")
         if status != 0:
-            raise RuntimeError("Compilation failed.")
+            raise RuntimeError(f"Compilation for function {fn.name()} failed.")
+        t_compile = time.time() - t_compile
+        print(f"Compilation time: {t_compile:.2f} seconds")
+        self.fn = cu.CusadiFunction(fn, num_envs)
+    
+    def __call__(self, inputs: list[torch.Tensor]) -> list[torch.Tensor]:
+        self.fn.evaluate(inputs)
+        torch.cuda.synchronize()
+        return self.fn.outputs_sparse
+
+def cusadi_fn(num_envs: int) -> Callable:
+    def decorator(fn_handle: Callable | ca.Function) -> Callable:
+        if not isinstance(fn_handle, ca.Function):
+            fn: ca.Function = fn_handle()
+        else:
+            fn: ca.Function = fn_handle
+        cu.generateCUDACodeDouble(fn)
+        # cu.generatePytorchCode(fn) # for easier debugging
+        cu.generateCMakeLists([fn], dtype_str="double")
+        
+        t_compile = time.time()
+        print("Compiling CUDA code...")
+        status = os.system(f"cd {cu.CUSADI_ROOT_DIR} && rm -rf build && mkdir -p build && cd build && cmake .. && make -j")
+        if status != 0:
+            raise RuntimeError(f"Compilation for function {fn.name()} failed.")
         t_compile = time.time() - t_compile
         print(f"Compilation time: {t_compile:.2f} seconds")
         
-        # Wrap the CasADi function with CusadiFunction
         cusadi_fn = cu.CusadiFunction(fn, num_envs)
         
-        # Define the wrapped function
         def wrapped(inputs: list[torch.Tensor]) -> list[torch.Tensor]:
             cusadi_fn.evaluate(inputs)
-            torch.cuda.synchronize()  # Ensure GPU computations are complete
+            torch.cuda.synchronize()
             return cusadi_fn.outputs_sparse
         
         return wrapped
@@ -57,15 +60,21 @@ def cusadi(num_envs: int) -> Callable:
 
 # test
 if __name__ == "__main__":
-
     def casadi_func() -> ca.Function:
         x = ca.SX.sym("x", 2) # type: ignore
         y = ca.SX.sym("y", 2) # type: ignore
         z = x + y
         return ca.Function("my_function", [x, y], [z])
 
-    N = 1
-    my_func = cusadi(N)(casadi_func())
+    mode = "decorator" # Options["decorator", "wrapper"]
+    N = 5
+    # decorator
+    if mode == "decorator":
+        my_func = cusadi_fn(num_envs=N)(casadi_func)
+    # wrapper
+    else:
+        my_casadi_func = casadi_func()
+        my_func = CusadiWrapper(my_casadi_func, num_envs=N)
 
     device = "cuda"
     inputs = [
